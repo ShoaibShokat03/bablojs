5c5536a8945d7c6c344758e341c1379afc8ae6a6
ShoaibShokat03 <shoaibshokat03@gmail.com>
2025-11-30 21:00:28 +0500

    some changes for template

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-#!/usr/bin/env node
-import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-const appName = process.argv[2];
-if (!appName) {
-  console.log("❌ App ka naam do!");
-  process.exit(1);
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const target = path.join(process.cwd(), appName);
-fs.mkdirSync(target);
-
-console.log(`🔥 Creating ${appName}...`);
-
-// Copy template files
-function copyFolder(src, dest) {
-  fs.mkdirSync(dest, { recursive: true });
-  for (const file of fs.readdirSync(src)) {
-    const srcPath = path.join(src, file);
-    const destPath = path.join(dest, file);
-    if (fs.lstatSync(srcPath).isDirectory()) copyFolder(srcPath, destPath);
-    else fs.copyFileSync(srcPath, destPath);
-  }
-}
-
-const templatePath = path.join(process.cwd(), "template");
+const templatePath = path.join(__dirname, "template"); // ✅ correct path
 copyFolder(templatePath, target);
-
-console.log(`✨ ${appName} created!`);
